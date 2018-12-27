7896b0297039c6166685baa352492f9702844dda
9QUICKSILVER9 <vovakozevnikov3@yandex.ru>
2018-12-27 12:50:49 +0300

    Changes

---
+++ Graphics/Project3/MyForm.h
@@ -0,0 +1,144 @@
+#pragma once
+#include <msclr/marshal_cppstd.h>
+#include "..\..\mp2-lab3-stack\TCalc.h"
+#include "..\..\mp2-lab3-stack\TStack.h"
+
+namespace Project3 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+
+
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label1;
+
+
+
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Myanmar Text", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(301, 28);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(112, 33);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"=";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"MS Reference Sans Serif", 9.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(12, 28);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(267, 33);
+			this->textBox1->TabIndex = 3;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"MS Reference Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::DeepPink;
+			this->label1->ImageAlign = System::Drawing::ContentAlignment::TopLeft;
+			this->label1->Location = System::Drawing::Point(443, 36);
+			this->label1->MaximumSize = System::Drawing::Size(100, 50);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(44, 15);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Solve";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::TopRight;
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(522, 102);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		std::string infix;
+		String^ str = textBox1->Text;
+		infix = msclr::interop::marshal_as<std::string>(str);
+		TCalc calc;
+		calc.SetInfix(infix);
+		calc.ToPostFix();
+		double res = calc.Calc();
+		label1->Text = Convert::ToString(res);
+		}
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+    private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+    }
+};
+}
---
+++ mp2-lab3-stack/main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include "TStack.h"
+#include "TCalc.h"
+using namespace std;
+
+bool Check(string str)
+{
+	TStack <char> st(10);
+
+	for (int i = 0; i < str.length(); i++)
+		if (str[i] == '(')
+			st.Push(str[i]);
+		else if (str[i] == ')')
+		{
+			if (st.IsEmpty())
+				return false;
+			else
+				st.Pop();
+		}
+	if (st.IsEmpty())
+		return true;
+	else
+		return false;
+}
+
+void main()
+ {
+	TCalc tcal;
+	string exp = "cos0";
+	tcal.SetInfix(exp);
+	cout << tcal.GetPostfix() << endl;;
+	cout << tcal.Calc() << endl;
+	system("pause");
+}
